9e5a81f2cc961d9ce9f41ec754228a7276aeb955
avit-io <alvise.vitturi@proton.me>
2026-05-14 21:15:54 +0000

    fix pdf index

--- flake.nix
+++ flake.nix
@@ -15,16 +15,11 @@
         {
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
-              (texlive.withPackages (ps: with ps; [ scheme-full latexmk ]))
+              texlive.combined.scheme-full
               ghostscript
               python3
               perl
-              glibcLocales
             ];
-
-            LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-            LANG = "en_US.UTF-8";
-            LC_ALL = "en_US.UTF-8";
 
             shellHook = ''
               echo "📄 LaTeX development environment"
